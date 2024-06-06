023d71358b7e4e20fdb3c1c8745078a2cf849be0
leandroffdev <leandrofrancagti@gmail.com>
2024-06-06 15:00:42 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// escopos
+var usuario = "leandro";
+let senha = 1234;
+const nome = 'Leandro';
+
+// Tipos
+let valor = Number(1000);
+let fracao = Number(1.45);
+let frase = String("Você consegue! Acredite e nunca desista!");
+let verdade = Boolean(true);
+
+//funções
+function funcao() {
+    console.log("Olá mundo!")
+}
+const funcao2 = () => {
+    console.log("Olá mundo!")
+}
+const funcao3 = function () {
+    console.log("Olá mundo!")
+}
+const arrowFunction = (2, 3) => x + y;
